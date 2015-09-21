64551636041c56fe6eefecbbf2a0022ed9cf2748
Bruce Steedman <MatzFan@users.noreply.github.com>
2015-09-21 13:49:55 +0100

    Update shoes.rb
    Update to v3.2.24

--- Casks/shoes.rb
+++ Casks/shoes.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'shoes' do
-  version '3.2.21'
-  sha256 'a2cdbba4f054c3c885d56038b4dd813c23957e1d0b1b084ca1e3a3d4bb9fcde1'
+  version '3.2.24'
+  sha256 'af6fc9576f6da3acacbd7a6ad1f8fbb4819e420712071defef08517755d8022f'
 
   # mvmanila.com is the official download host per the vendor homepage
   url "http://shoes.mvmanila.com/public/shoes/shoes-#{version}-osx-10.9.tgz"
